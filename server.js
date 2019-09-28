7dca3f3c9b1b8cf48fed297006c6b94aeb263b10
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-09-28 10:59:39 -0600

    code

--- server.js
+++ server.js
@@ -3,29 +3,25 @@ const app = express();
 const path = require("path")
 const mongoose = require("mongoose")
 const port = process.env.PORT || 5000;
+const routes = require("./routes");
 
 // init DB
 require('dotenv').config()
 
-var db = require("./models");
-var MONGODB_URI = process.env.MONGODB_URI;
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true ,useUnifiedTopology: true})
-.then(()=>console.log("DB connected"));
-
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  // Serve static files from the React app
+     app.use(express.static(path.join(__dirname, 'client/build')));
+}
+// Add routes, both API and view
+app.use(routes);
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// -- API Routes 
-app.use('./routes')
-
-// -- Catch All Route
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist", { useNewUrlParser: true ,useUnifiedTopology: true})
+.then(()=>console.log("DB connected"));
 
 // console.log that server is up and running
 app.listen(port, () => console.log(`Listening on  http://localhost:${port}/`));
